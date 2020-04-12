2e5a2e3516cb803cc4ec80163e9e6cc041488885
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-04-11 21:04:54 -0400

    Tweaks

--- scripts/tag_prediction.py
+++ scripts/tag_prediction.py
@@ -36,7 +36,10 @@ def create_logistic_model(vocab_tokens_size, vocab_tags_size):
 
 
 def train(vocab_tokens_size=10000, vocab_tags_size=500):
-
+    # Hyper-parameters 
+    input_size = 28 * 28    # 784
+    num_classes = 10
+    num_epochs = 5
     batch_size = 100
     learning_rate = 0.001
 
@@ -45,7 +48,54 @@ def train(vocab_tokens_size=10000, vocab_tags_size=500):
 
 
     train_dataset = stackoverflow('/users/xzhu/tag/stackoverflow_tf/', True)
+    test_dataset = stackoverflow('/users/xzhu/tag/stackoverflow_tf/', False)
     #print(train_dataset.__getitem__(0)[0].size())
+
+    # Data loader (input pipeline)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                            batch_size=batch_size, 
+                                            shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                            batch_size=batch_size, 
+                                            shuffle=False)
+
+    # Loss and optimizer
+    # nn.CrossEntropyLoss() computes softmax internally
+    criterion = nn.CrossEntropyLoss()  
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
+
+    # Train the model
+    total_step = len(train_loader)
+    for epoch in range(num_epochs):
+        for i, (text, labels) in enumerate(train_loader):
+
+            # Forward pass
+            outputs = model(text)
+            loss = criterion(outputs, labels)
+            
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            if (i+1) % 100 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+    # # Test the model
+    # # In test phase, we don't need to compute gradients (for memory efficiency)
+    # with torch.no_grad():
+    #     correct = 0
+    #     total = 0
+    #     for images, labels in test_loader:
+    #         images = images.reshape(-1, input_size)
+    #         outputs = model(images)
+    #         _, predicted = torch.max(outputs.data, 1)
+    #         total += labels.size(0)
+    #         correct += (predicted == labels).sum()
+
+    #     print('Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
 
 
 
