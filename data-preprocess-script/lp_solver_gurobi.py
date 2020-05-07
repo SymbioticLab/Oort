b7e66ab4e4b9b0f135b7c7d775dff8d3e52e8e29
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-05-07 17:39:39 -0400

    Tweaks on lp solver

---
+++ data-preprocess-script/lp_solver_gurobi.py
@@ -0,0 +1,35 @@
+import gurobipy as gp
+from gurobipy import GRB
+
+try:
+
+    # Create a new model
+    m = gp.Model("mip1")
+
+    # Create variables
+    x = m.addVar(vtype=GRB.BINARY, name="x")
+    y = m.addVar(vtype=GRB.BINARY, name="y")
+    z = m.addVar(vtype=GRB.BINARY, name="z")
+
+    # Set objective
+    m.setObjective(x + y + 2 * z, GRB.MAXIMIZE)
+
+    # Add constraint: x + 2 y + 3 z <= 4
+    m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
+
+    # Add constraint: x + y >= 1
+    m.addConstr(x + y >= 1, "c1")
+
+    # Optimize model
+    m.optimize()
+
+    for v in m.getVars():
+        print('%s %g' % (v.varName, v.x))
+
+    print('Obj: %g' % m.objVal)
+
+except gp.GurobiError as e:
+    print('Error code ' + str(e.errno) + ': ' + str(e))
+
+except AttributeError:
+    print('Encountered an attribute error')
